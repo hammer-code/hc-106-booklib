5813399d9e7dbccb7955580ad5e459d352d14d7b
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-12 16:38:02 +0800

    create borroweds views

--- booklib/routes.py
+++ booklib/routes.py
@@ -79,6 +79,33 @@ def books_edit(book_id):
   }
   return render_template("books_edit.html", book=book)
 
+@app.route("/borroweds")
+def borroweds_index():
+  borroweds = [
+    {
+      "receipt_number": "ABCDE12345",
+      "book" : {
+        "id": 1,
+        "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1287493789l/179133.jpg",
+        "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
+        "author": "Evans, Eric",
+        "published": "20 Agustus 2003",
+        "quantity": 1
+      },
+      "student": {
+        "id": 1,
+        "name": "Milch",
+        "number": "ABCDEF"
+      }
+    }
+  ]
+
+  return render_template("borroweds_index.html", borroweds=borroweds)
+
+@app.route("/borroweds/create")
+def borroweds_create():
+  return render_template("borroweds_create.html")
+
 @app.route("/register")
 def register():
   return render_template("register.html")
