3e1c3dd4475fe79380d00a21fb2e2bd73337d298
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-12 12:10:17 +0800

    create my_library view

--- booklib/routes.py
+++ booklib/routes.py
@@ -14,6 +14,19 @@ def index():
   ]
   return render_template("index.html", books=books)
 
+@app.route("/my_library")
+def my_library():
+  books = [
+    {
+      "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1287493789l/179133.jpg",
+      "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
+      "author": "Evans, Eric",
+      "published": "20 Agustus 2003",
+      "status": "Dikembalikan"
+    }
+  ]
+  return render_template("my_library.html", books=books)
+
 @app.route("/authors")
 def authors_index():
   authors = [
