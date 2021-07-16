bcd40bb7548b067c5dc0fe2a8b76fa01db530d17
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-16 16:45:59 +0800

    add secret key for flash function

--- booklib/main.py
+++ booklib/main.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from booklib.controllers import main, author, book, borrowed
 from booklib.command import register_command
 
 def create_app():
   app = Flask(__name__)
-  app.secret_key = b'2naAEqscuqk094HHyCiP'
+  app.secret_key = os.getenv('SECRET_KEY')
   app.register_blueprint(main.bp)
   app.register_blueprint(author.bp)
   app.register_blueprint(book.bp)
