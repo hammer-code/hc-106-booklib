e87d9edd0e6b76f9442a3e0c1c099aec6f663df4
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-16 19:34:43 +0800

    modify transform for book

--- booklib/repositories/book.py
+++ booklib/repositories/book.py
@@ -1,5 +1,6 @@
 from .base import Repository
-
+from .book_author import BookAuthorRepository
+from .author import AuthorRepository
 class BookRepository(Repository):
   def __init__(self, cnx):
     Repository.__init__(self, cnx)
@@ -8,3 +9,19 @@ class BookRepository(Repository):
       "id", "title", "published", "quantity",
       "image_url", "created_at", "updated_at",
     )
+
+  def transform(self, columns, row):
+    result = super().transform(columns, row)
+
+    book_author_repo = BookAuthorRepository(self.cnx)
+    book_authors = book_author_repo.filter({"book_id": row[0]})
+
+    author_repo = AuthorRepository(self.cnx)
+    authors = [
+      author_repo.filter_by_id(book_author["author_id"])
+      for book_author in book_authors
+    ]
+    result["authors"] = authors
+    result["authors_num"] = len(authors)
+
+    return result
