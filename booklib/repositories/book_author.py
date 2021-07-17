4438f4e58a06b351852b607fc7ffe04202263da6
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-17 14:56:13 +0800

    fix book author init parent class

--- booklib/repositories/book_author.py
+++ booklib/repositories/book_author.py
@@ -2,7 +2,7 @@ from .base import Repository
 
 class BookAuthorRepository(Repository):
   def __init__(self, cnx):
-    self.cnx = cnx
+    Repository.__init__(self, cnx)
     self.table_name = "book_authors"
     self.columns = (
       "id", "book_id", "author_id", "created_at", "updated_at",
