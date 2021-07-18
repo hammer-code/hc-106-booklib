e560f5cb93ea7b55004a92c91b840de4fc2ce612
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-18 19:41:27 +0800

    add auth blueprint

--- booklib/main.py
+++ booklib/main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-from booklib.controllers import main, author, book, borrowed
+from booklib.controllers import main, auth, author, book, borrowed
 
 def create_app():
   app = Flask(__name__)
   app.register_blueprint(main.bp)
+  app.register_blueprint(auth.bp)
   app.register_blueprint(author.bp)
   app.register_blueprint(book.bp)
   app.register_blueprint(borrowed.bp)
