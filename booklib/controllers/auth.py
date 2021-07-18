1da9be2d7c64409915ddacf444d4a45d0c8bad60
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-18 20:14:44 +0800

    delete method

--- booklib/controllers/auth.py
+++ booklib/controllers/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-@bp.route("/register", methods=["GET", "POST"])
+@bp.route("/register")
 def register():
   return render_template("auth/register.html")
 
