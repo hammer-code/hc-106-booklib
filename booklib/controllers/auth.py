8c22a8796e0ffbaead66e0fbe53f9a653e34ad7c
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-18 20:03:46 +0800

    add new line

--- booklib/controllers/auth.py
+++ booklib/controllers/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
 @bp.route("/register", methods=["GET", "POST"])
 def register():
   return render_template("auth/register.html")
