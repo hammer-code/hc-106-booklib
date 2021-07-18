e044f6b63a47dd3bb259c2850d5fbe4de28910da
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-18 20:03:28 +0800

    delete unused code

--- booklib/controllers/auth.py
+++ booklib/controllers/auth.py
@@ -1,7 +1,4 @@
-import os
-from flask import Blueprint, render_template, request
-from booklib.db import get_db
-from booklib.repositories import UserRepository
+from flask import Blueprint, render_template
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 @bp.route("/register", methods=["GET", "POST"])
