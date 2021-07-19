d286739246a8f0cb3850bad397e30a00a0e69240
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-19 11:52:08 +0800

    create register

--- booklib/controllers/auth.py
+++ booklib/controllers/auth.py
@@ -1,9 +1,49 @@
-from flask import Blueprint, render_template
+from flask import (
+  Blueprint, render_template, request, flash, redirect
+)
+from flask_bcrypt import  generate_password_hash
+from booklib.repositories import UserRepository, StudentRepository
+from booklib.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-@bp.route("/register")
+@bp.route("/register", methods=["GET", "POST"])
 def register():
+  if request.method == "POST":
+    cnx = get_db()
+    student_name = request.form["name"]
+    student_number = request.form["number"]
+    password = request.form["password"]
+
+    if not student_name:
+      flash("Student name is required", "error")
+    if not student_number:
+      flash("Student number is required", "error")
+    if not password:
+      flash("Password is required", "error")
+    
+    if student_name and student_number and password:
+      user_repo = UserRepository(cnx)
+      user_exists = user_repo.filter({"username": student_number})
+      if not user_exists:
+        user = user_repo.create({
+          "username": student_number,
+          "password": generate_password_hash(password).decode("utf-8"),
+          "role": "student"
+        })
+
+        student_repo = StudentRepository(cnx)
+        student = student_repo.create({
+          "user_id": user["id"],
+          "name": student_name,
+          "number": student_number,
+        })
+        flash("You are registered!", "success")
+
+        return redirect("/my_library")
+      else:
+        flash("Student already exists", "error")
+
   return render_template("auth/register.html")
 
 @bp.route("/login")
--- booklib/repositories/__init__.py
+++ booklib/repositories/__init__.py
@@ -1,5 +1,5 @@
 from .author import AuthorRepository
 from .book import BookRepository
 from .book_author import BookAuthorRepository
-from .student import StudentRepository
 from .user import UserRepository
+from .student import StudentRepository
