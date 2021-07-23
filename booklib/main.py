fd1da20e28d2f1668af53c1984d4182a6c713d44
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-23 10:35:06 +0800

    delete unused code

--- booklib/main.py
+++ booklib/main.py
@@ -1,14 +1,13 @@
 import os
 from flask import Flask
-from werkzeug.local import LocalProxy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from booklib.controllers import main, auth, author, book, borrowed
 from booklib.command import register_command
 
 def create_app():
   app = Flask(__name__)
   app.secret_key = os.getenv('SECRET_KEY')
+  app.config["UPLOAD_FOLDER"] = os.getenv('UPLOAD_FOLDER')
   app.register_blueprint(main.bp)
   app.register_blueprint(auth.bp)
   app.register_blueprint(author.bp)
@@ -17,6 +16,4 @@ def create_app():
   register_command(app)
   with app.app_context():
     app.bcrypt = Bcrypt(app)
-    app.login_manager = LoginManager()
-    app.login_manager.init_app(app)
   return app
