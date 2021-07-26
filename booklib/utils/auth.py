aac4188e3bd009dafb558576868de948f427eab2
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-26 12:24:49 +0800

    show username if user is logged in

--- booklib/utils/auth.py
+++ booklib/utils/auth.py
@@ -2,20 +2,18 @@ from flask import session, g, redirect, abort
 from booklib.db import get_db
 from booklib.repositories import UserRepository
 
+user_repo = UserRepository()
+
 def load_user(app):
   @app.before_request
   def before_request():
-    with app.app_context():
-      if "user_id" in session:
-        user_repo = UserRepository(get_db())
-        user = user_repo.filter_by_id(session["user_id"])
-        del user["password"]
-        g.user = user
-        
+    if "user_id" in session:
+      user = user_repo.filter_by_id(session["user_id"])
+      del user["password"]
+      g.user = user
 
 def is_authenticated(func):
   if "user_id" in session:
-      user_repo = UserRepository(get_db())
       user = user_repo.filter_by_id(session["user_id"])
       
       if len(user) > 0:
@@ -25,7 +23,6 @@ def is_authenticated(func):
 
 def is_admin(func):
   if "user_id" in session:
-    user_repo = UserRepository(get_db())
     user = user_repo.filter_by_id(session["user_id"])
     
     if len(user) > 0:
@@ -38,7 +35,6 @@ def is_admin(func):
 
 def is_student(func):
   if "user_id" in session:
-    user_repo = UserRepository(get_db())
     user = user_repo.filter_by_id(session["user_id"])
     
     if len(user) > 0:
