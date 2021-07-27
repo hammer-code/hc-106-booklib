c94bdc76e3947e73ce94a57c352fc8ddd09dfccc
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-27 12:25:29 +0800

    change book edit

--- booklib/controllers/book.py
+++ booklib/controllers/book.py
@@ -32,15 +32,36 @@ def create():
 
 @bp.route("/edit/<int:book_id>")
 def edit(book_id):
+  authors = [
+    {
+      "id": 1,
+      "name": "Evans"
+    },
+    {
+      "id": 2,
+      "name": "Eric"
+    }
+  ]
+
   book = {
     "id": 1,
     "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1287493789l/179133.jpg",
     "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
-    "author": "Evans, Eric",
+    "authors": [
+      {
+        "id": 1,
+        "name": "Evans"
+      },
+      {
+        "id": 2,
+        "name": "Eric"
+      }
+    ],
     "published": "20 Agustus 2003",
     "quantity": 1
   }
-  return render_template("book/edit.html", book=book)
+  
+  return render_template("book/edit.html", book=book, authors=authors)
 
 @bp.route("/delete/<int:book_id>")
 def delete(book_id):
