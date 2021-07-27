b510be5d6a3dea1af5f5c9a01aeed15640a25352
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-27 10:24:55 +0800

    create authorization

--- booklib/utils/auth.py
+++ booklib/utils/auth.py
@@ -1,3 +1,4 @@
+from functools import wraps
 from flask import session, g, redirect, abort, request
 from booklib.db import get_db
 from booklib.repositories import UserRepository
@@ -12,43 +13,47 @@ def load_user(app):
       del user["password"]
       g.user = user
 
-def is_guest(func):
-  if "user_id" not in session:
-    func()
-  
-  return redirect("/")
+def is_guest(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if "user_id" not in session:
+      f(*args, **kwargs)
+    return redirect("/")
 
-def is_authenticated(func):
-  if "user_id" in session:
-    user = user_repo.filter_by_id(session["user_id"])
-    
-    if len(user) > 0:
-      func()
-    
-    abort(403)
+  return decorated_function
+
+def is_authenticated(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if "user_id" in session:
+      if len(g.user) > 0:
+        f(*args, **kwargs)
       
-  redirect("/auth/login")
+      abort(403)
+    redirect("/auth/login")
 
-def is_admin(func):
-  if "user_id" in session:
-    user = user_repo.filter_by_id(session["user_id"])
-    
-    if len(user) > 0:
-      if user["role"] == "admin":
-        func()
+  return decorated_function
 
-    abort(403)
+def is_admin(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if "user_id" in session:
+      if len(g.user) > 0:
+        if g.user["role"] == "admin":
+          f(*args, **kwargs)
+        abort(403)
+      redirect("/auth/login")
 
-  redirect("/auth/login")
+  return decorated_function
 
-def is_student(func):
-  if "user_id" in session:
-    user = user_repo.filter_by_id(session["user_id"])
-    
-    if len(user) > 0:
-      if user["role"] == "student":
-        func()
-    
-    abort(403)
+def is_student(f):
+  @wraps(f)
+  def decorated_function(*args, **kwargs):
+    if "user_id" in session:
+      if len(g.user) > 0:
+        if g.user["role"] == "student":
+          f()
+      abort(403)
+    redirect("/auth/login")
 
-  redirect("/auth/login")
+  return decorated_function
