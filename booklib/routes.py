ee257a56cfba00898afcc387d89fa9eeaa8ee866
Arif Suganda <arifsuganda.su@gmail.com>
2021-07-28 12:53:03 +0800

    black formatted

--- booklib/routes.py
+++ booklib/routes.py
@@ -1,16 +1,20 @@
 from booklib import app
 from flask import render_template
 
+
 @app.route("/")
 def index():
-  books = [
-    {
-      "id": 1,
-      "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1287493789l/179133.jpg",
-      "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
-      "author": "Evans, Eric",
-      "published": "20 Agustus 2003",
-      "quantity": 1
-    }
-  ]
-  return render_template("index.html", books=books)
+    books = [
+        {
+            "id": 1,
+            "image_url": (
+                "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com",
+                "/books/1287493789l/179133.jpg",
+            ),
+            "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
+            "author": "Evans, Eric",
+            "published": "20 Agustus 2003",
+            "quantity": 1,
+        }
+    ]
+    return render_template("index.html", books=books)
