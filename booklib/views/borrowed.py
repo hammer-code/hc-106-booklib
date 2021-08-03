1226cab21cedb4f3c8d014d7dbb18296f93c74c4
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-03 13:40:11 +0800

    borrowed fix crud

--- booklib/views/borrowed.py
+++ booklib/views/borrowed.py
@@ -26,26 +26,46 @@ def create():
         if not student:
             flash("Student is not found", "error")
         if book and student:
-            borrowed_repo.create(
-                {
-                    "receipt_number": generate_random_string(16),
-                    "book_id": book[0]["id"],
-                    "student_id": student[0]["id"],
-                }
-            )
-            flash("The book has been borrowed", "success")
-            return redirect("/borroweds")
+            if book[0]["quantity"] > 0:
+                borrowed_repo.create(
+                    {
+                        "receipt_number": generate_random_string(16),
+                        "book_id": book[0]["id"],
+                        "student_id": student[0]["id"],
+                    }
+                )
+                book_repo.update(book[0]["id"], {"quantity": book[0]["quantity"] - 1})
+                flash("The book has been borrowed", "success")
+                return redirect("/borroweds")
+            flash("There is no book left which can be borrowed", "error")
     return render_template("borrowed/create.html")
 
 
-@bp.route("/returned/<int:borrowed_id>")
-def returned():
-    pass
+@bp.route("/returned/<int:borrowed_id>", methods=["GET", "POST"])
+def returned(borrowed_id):
+    if request.method == "POST":
+        borrowed = borrowed_repo.filter_by_id(borrowed_id)
+        if not borrowed["returned"]:
+            borrowed_repo.update(borrowed_id, {"returned": True})
+            book = book_repo.filter_by_id(borrowed["book_id"])
+            book_repo.update(
+                borrowed["book_id"], {"quantity": book["quantity"] + 1}
+            )
+            flash("Book is returned", "success")
+            return redirect("/borroweds")
+        flash("Book had been returned", "error")
+    return redirect("/borroweds")
 
 
-@bp.route("/delete/<int:borrowed_id>")
-def delete():
-    pass
+@bp.route("/delete/<int:borrowed_id>", methods=["GET", "POST"])
+def delete(borrowed_id):
+    borrowed = borrowed_repo.filter_by_id(borrowed_id)
+    if not borrowed["returned"]:
+        book = book_repo.filter_by_id(borrowed["book_id"])
+        book_repo.update(borrowed["book_id"], {"quantity": book["quantity"] + 1})
+    borrowed_repo.delete(borrowed_id)
+    flash("Borrowed is deleted", "success")
+    return redirect("/borroweds")
 
 
 @bp.route("/books")
