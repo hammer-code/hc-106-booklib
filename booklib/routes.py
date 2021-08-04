a039f9453e1bdcc5e9df862e1955d1b746e2b417
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-04 13:41:25 +0800

    change authors format

--- booklib/routes.py
+++ booklib/routes.py
@@ -71,7 +71,7 @@ def books_index():
             "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com"
             + "/books/1287493789l/179133.jpg",
             "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
-            "author": "Evans, Eric",
+            "authors": [{"name": "Evans"}, {"name": "Eric"}],
             "published": "20 Agustus 2003",
             "quantity": 1,
         }
@@ -120,7 +120,7 @@ def borroweds_index():
                 "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com"
                 + "/books/1287493789l/179133.jpg",
                 "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
-                "author": "Evans, Eric",
+                "authors": [{"name": "Evans"}, {"name": "Eric"}],
                 "published": "20 Agustus 2003",
                 "quantity": 1,
             },
