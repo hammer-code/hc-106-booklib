9a3beeb9ce8b12d84f85b6c726b016f88cd42cb3
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-04 15:02:18 +0800

    fix borrowed

--- booklib/views/borrowed.py
+++ booklib/views/borrowed.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 bp = Blueprint("borroweds", __name__, url_prefix="/borroweds")
 
@@ -11,7 +11,10 @@ def index():
             "receipt_number": "ABCDE12345",
             "book": {
                 "id": 1,
-                "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1287493789l/179133.jpg",
+                "image_url": (
+                    "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com",
+                    "/books/1287493789l/179133.jpg",
+                ),
                 "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
                 "author": "Evans, Eric",
                 "published": "20 Agustus 2003",
@@ -19,6 +22,8 @@ def index():
             },
             "student": {"id": 1, "name": "Milch", "number": "ABCDEF"},
             "created_at": "2021-07-01",
+            "updated_at": "2021-08-01",
+            "returned": 1,
         }
     ]
     return render_template("borrowed/index.html", borroweds=borroweds)
@@ -37,3 +42,25 @@ def returned():
 @bp.route("/delete/<int:borrowed_id>")
 def delete():
     pass
+
+
+@bp.route("/books")
+def books_search():
+    books = [
+        {
+            "id": 1,
+            "image_url": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com"
+            "/books/1287493789l/179133.jpg",
+            "title": "Domain-Driven Design: Tackling Complexity in the Heart of Software",
+            "authors": [{"id": 1, "name": "Evans"}, {"id": 2, "name": "Eric"}],
+            "published": "20 Agustus 2003",
+            "quantity": 1,
+        }
+    ]
+    return jsonify(books)
+
+
+@bp.route("/students")
+def students_search():
+    students = [{"number": "ABCDE1234", "name": "Hammer Code"}]
+    return jsonify(students)
