716758b58240753963c19d3b887466d73f233ddd
Arif Suganda <arifsuganda.su@gmail.com>
2021-08-06 16:23:05 +0800

    on save formatter

--- booklib/utils/main.py
+++ booklib/utils/main.py
@@ -1,16 +1,18 @@
 import random
 import string
 
+
 def generate_random_string(k):
-  return "".join(random.choices(
-    string.ascii_uppercase + string.ascii_lowercase +
-    string.digits, k=k)
-  )
+    return "".join(
+        random.choices(
+            string.ascii_uppercase + string.ascii_lowercase + string.digits, k=k
+        )
+    )
+
 
 def allowed_file(filename, allowed_extensions):
-  return '.' in filename and \
-    get_extension(filename) \
-    in allowed_extensions
+    return "." in filename and get_extension(filename) in allowed_extensions
+
 
 def get_extension(filename):
-  return filename.rsplit('.', 1)[1].lower()
+    return filename.rsplit(".", 1)[1].lower()
